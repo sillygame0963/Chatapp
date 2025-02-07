e310d51ae535aa16f8386f300c9e0fd70db5fb6c
Nguyen Thai Son <sillygame0963@gmail.com>
2025-02-07 18:47:55 +0700

    'test

--- server/index.js
+++ server/index.js
@@ -18,6 +18,6 @@ app.get('/', (req, res) => {
     })
 })  
 
-app.listen(PORT, () => {
-    console.log("Server is running on port" + PORT)
-})
+// app.listen(PORT, () => {
+//     console.log("Server is running on port" + PORT)
+// })
